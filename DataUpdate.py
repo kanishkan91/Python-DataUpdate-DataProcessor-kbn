fb8eb608415a37da96e82c28d76c6785e2dea6c3
Kanishka Narayan <37234086+kanishkan91@users.noreply.github.com>
2019-09-07 22:02:35 -0400

    See description for changes
    1. Changed WDI, FAO,UIS to chunking process from dash
    2. Updated output files with edited col names
    3. Updated IHME function to include sex
    4. Updated outputs to include SQLLite file

--- DataUpdate.py
+++ DataUpdate.py
@@ -27,11 +27,11 @@ def IHMEDetailedDeathsData():
     import glob
     import dask as dd
     print(
-        r"Please make sure the zip file Pythonfiles.zip is saved under C:\Users\Public\ and that you have run the installation code in case you are a first time user")
-    print(r"Reading files from C:\Users\Public\Pythonfiles")
-    CountryConcord = pd.read_csv(r'C:\\Users\Public\Pythonfiles\CountryConcordIHME.csv')
-    SeriesConcord = pd.read_csv(r'C:\\Users\Public\Pythonfiles\SeriesConcordIHME.csv')
-    path = (r'C:\Users\Public\Pythonfiles\IHMEDownloads\DetailedDeathFileData')
+        r"Please make sure the zip file Pythonfiles.zip is saved under input and that you have run the installation code in case you are a first time user")
+    print(r"Reading files from input")
+    CountryConcord = pd.read_csv(r'input\CountryConcordIHME.csv')
+    SeriesConcord = pd.read_excel(r'input\SeriesConcordIHME.xlsx')
+    path = (r'input\\IHMEDownloads\DetailedDeathFileData')
     filenames = glob.glob(path + "/*.csv")
     GBDData = []
     for filename in filenames:
@@ -44,11 +44,13 @@ def IHMEDetailedDeathsData():
         GBDData.append(filename)
     GBDData = pd.concat(GBDData, ignore_index=True)
     GBDDeaths = GBDData
-    p = pd.pivot_table(GBDDeaths, index=["Country name in IFs", "sex_id", 'Series name in IFs', 'metric_name'],
+    data = pd.pivot_table(GBDDeaths, index=["Country name in IFs", "sex_id", 'Series name in IFs', 'metric_name'],
                        values=['val'],
                        columns=["age_name"], aggfunc=[np.sum])
-
-    return (p)
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'val',", "").replace(")", "").replace("'", "") \
+                    for hdr in data.columns]
+    return (data)
 
 
 def IHMEDetailedDeathsFile():
@@ -70,10 +72,10 @@ def IHMEDetailedDeathsFile():
     p.to_excel(writer, sheet_name='Deaths', merge_cells=False)
 
     writer.save()
-    print(r"File Saved Under C:\Users\Public\PythonScripts")
+    print(r"File Saved")
 
 
-def IHMEHistoricalDeathData():
+def IHMEHistoricalDeathData(sex_name):
     import numpy as np
     import requests
     import matplotlib.pyplot as plt
@@ -87,10 +89,10 @@ def IHMEHistoricalDeathData():
     import glob
     import dask as dd
 
-    CountryConcord = pd.read_csv(r'C:\\Users\Public\Pythonfiles\CountryConcordIHME.csv')
-    SeriesConcord = pd.read_csv(r'C:\\Users\Public\Pythonfiles\SeriesConcordIHME.csv')
+    CountryConcord = pd.read_csv(r'input\CountryConcordIHME.csv',encoding="ISO-8859-1")
+    SeriesConcord = pd.read_excel(r'input\SeriesConcordIHME.xlsx')
 
-    path = (r'C:\Users\Public\Pythonfiles\IHMEDownloads\HistDeathFileData')
+    path = (r'input\\IHMEDownloads\HistDeathFileData')
 
     filenames = glob.glob(path + "/*.csv")
     print("Reading files from path" + str(path))
@@ -100,19 +102,26 @@ def IHMEHistoricalDeathData():
         filename = pd.read_csv(filename)
         filename = pd.merge(filename, CountryConcord, how='left', left_on='location_name',
                             right_on='Country name in IHME')
+
         filename = pd.merge(filename, SeriesConcord, how='left', left_on='cause_name', right_on='Series name in IHME')
         filename = filename.dropna(how='any')
-        # GBDDalys.append(pd.read_csv(filename,low_memory=False))
+        filename = filename.loc[filename['sex_name'] == sex_name]
         GBDData.append(filename)
+
+
 
     GBDData = pd.concat(GBDData, ignore_index=True)
 
+
     GBDDeaths = GBDData
     print(GBDDeaths.head())
-    p = pd.pivot_table(GBDDeaths, index=["Country name in IFs", 'year'], values=['val'],
+    data = pd.pivot_table(GBDDeaths, index=["Country name in IFs", 'year'], values=['val'],
                        columns=["Series name in IFs"], aggfunc=[np.sum])
 
-    return (p)
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'val',", "").replace(")", "").replace("'", "") \
+                    for hdr in data.columns]
+    return (data)
 
 
 def IHMEHistoricalDeathsFile():
@@ -148,10 +157,10 @@ def FAOFBS():
     import dask.dataframe as dd
     import statsmodels.api as sm
 
-    Country_Concord = pd.read_csv('C:\\Users\Public\Pythonfiles\CountryConcordFAO.csv', encoding="ISO-8859-1")
-    concord_table = pd.read_csv('C:\\Users\Public\Pythonfiles\Aggregation for crop type.csv')
-    series_concord_table = pd.read_csv('C:\\Users\Public\Pythonfiles\FAOSeriesConcordance.csv')
-    data = pd.read_csv(r'C:\\Users\Public\Pythonfiles\FoodBalanceSheets_E_All_Data_(Normalized).csv',
+    Country_Concord = pd.read_csv('input\CountryConcordFAO.csv', encoding="ISO-8859-1")
+    concord_table = pd.read_csv('input\Aggregation for crop type.csv')
+    series_concord_table = pd.read_csv('input\FAOSeriesConcordance.csv')
+    data = pd.read_csv(r'input\FoodBalanceSheets_E_All_Data_(Normalized).csv',
                        encoding="ISO-8859-1",chunksize=100000)
 
     chunk_list=[]
@@ -171,9 +180,11 @@ def FAOFBS():
         ['Area Code', 'Item Code', 'Flag', 'Unit', 'Year Code', 'Element', 'Element Code', 'Code', 'Code Name', 'Item',
          'Code no'], axis=1)
 
-    p = pd.pivot_table(data, index=["Area", 'Year'], values=['Value'], columns=["Series_Name"], aggfunc=[np.sum])
-
-    return (p)
+    data = pd.pivot_table(data, index=["Area", 'Year'], values=['Value'], columns=["Series_Name"], aggfunc=[np.sum])
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'Value',", "").replace(")", "").replace("'", "") \
+                    for hdr in data.columns]
+    return (data)
 
 
 def FAOFBSFile():
@@ -209,10 +220,10 @@ def FAOFBSFish():
     import dask.dataframe as dd
     import statsmodels.api as sm
 
-    Country_Concord = pd.read_csv('C:\\Users\Public\Pythonfiles\CountryConcordFAO.csv', encoding="ISO-8859-1")
-    data = pd.read_csv('C:\\Users\Public\Pythonfiles\FoodBalanceSheets_E_All_Data_(Normalized).csv',
+    Country_Concord = pd.read_csv('input\CountryConcordFAO.csv', encoding="ISO-8859-1")
+    data = pd.read_csv('input\FoodBalanceSheets_E_All_Data_(Normalized).csv',
                        encoding="ISO-8859-1",chunksize=100000)
-    concord_table = pd.read_csv('C:\\Users\Public\Pythonfiles\AggregationforFish.csv')
+    concord_table = pd.read_csv('input\AggregationforFish.csv')
     chunk_list = []
 
     for chunk in data:
@@ -236,9 +247,11 @@ def FAOFBSFish():
     #datapanda = data.groupby(["Area","Year","Variable"]).sum().compute()
     #print(datapanda.head())
 
-    p = pd.pivot_table(data, index=["Country name in IFs", 'Year'], values=['Value'], columns=["Variable"], aggfunc=[np.sum])
-
-    return (p)
+    data = pd.pivot_table(data, index=["Country name in IFs", 'Year'], values=['Value'], columns=["Variable"], aggfunc=[np.sum])
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'Value',", "").replace(")", "").replace("'", "") \
+                    for hdr in data.columns]
+    return (data)
 
 
 def FAOFBSFishFile():
@@ -279,8 +292,8 @@ def UISData():
     from lxml import html
 
     print(r'Warning: The UIS Pull may take a long time to complete. Please do not close this session')
-    dict = pd.read_excel(r'C:\\Users\Public\Pythonfiles\IndicatorNoUIS.xlsx', sheet_name='Master')
-    Yeardict = pd.read_excel(r'C:\Users\Public\Pythonfiles\YearDictUIS.xlsx', sheet_name='Sheet1')
+    dict = pd.read_excel(r'input\IndicatorNoUIS.xlsx', sheet_name='Master')
+    Yeardict = pd.read_excel(r'input\\YearDictUIS.xlsx', sheet_name='Sheet1')
     SeriesCode = []
     Countryname = []
     Year = []
@@ -314,10 +327,7 @@ def UISData():
                 Value.append(Val1)
 
     print('loop complete')
-    print(len(SeriesCode))
-    print(len(Countryname))
-    print(len(Year))
-    print(len(Value))
+
 
     test_df = pd.DataFrame.from_dict({'SeriesName': SeriesCode,
                                       'Country': Countryname,
@@ -325,22 +335,24 @@ def UISData():
                                       'Value': Value}, orient='index')
     df = test_df.transpose()
 
-    CountryconcordUIS = pd.read_excel(r'C:\\Users\Public\Pythonfiles\UISCountryConcord.xlsx', sheet_name='Sheet1')
-    SeriesConcordUIS = pd.read_excel(r'C:\\Users\Public\Pythonfiles\UIS.xlsx', sheet_name='Sheet1')
-    print(df.head())
+    CountryconcordUIS = pd.read_excel(r'input\UISCountryConcord.xlsx', sheet_name='Sheet1')
+    SeriesConcordUIS = pd.read_excel(r'input\UIS.xlsx', sheet_name='Sheet1')
+
 
     df = pd.merge(df, CountryconcordUIS, left_on='Country', right_on='Country code name', how='left')
     df = pd.merge(df, SeriesConcordUIS, left_on='SeriesName', right_on='Indicator Number', how='left')
 
-    print(df.head())
+
 
     df = df[pd.notnull(df['Country name in IFs'])]
     df = df[pd.notnull(df['Series Name in IFs'])]
 
-    p = pd.pivot_table(df, index=['Country name in IFs', 'Year'], columns=['Series Name in IFs'], values=['Value'],
+    data = pd.pivot_table(df, index=['Country name in IFs', 'Year'], columns=['Series Name in IFs'], values=['Value'],
                        aggfunc=[np.sum])
-
-    return (p)
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'Value',", "").replace(")", "").replace("'", "") \
+                    for hdr in data.columns]
+    return (data)
 
 
 def UISDataFile():
@@ -366,13 +378,13 @@ def AQUASTATData():
     import dask.dataframe as dd
     import statsmodels.api as sm
 
-    data = pd.read_excel('C:\\Users\Public\Pythonfiles\AQUASTAT.xlsx')
+    data = pd.read_excel('input\AQUASTAT.xlsx')
 
-    country_concordance = pd.read_excel('C:\\Users\Public\Pythonfiles\CountryConcordanceAQUASTAT.xlsx')
+    country_concordance = pd.read_excel('input\CountryConcordanceAQUASTAT.xlsx')
 
     data = pd.merge(data, country_concordance, how="left", left_on="Area", right_on="Area name")
 
-    series_concordance = pd.read_excel('C:\\Users\Public\Pythonfiles\SeriesConcordanceAQUASTAT.xlsx')
+    series_concordance = pd.read_excel('input\SeriesConcordanceAQUASTAT.xlsx')
 
     # print (series_concordance.head())
     # print(data.head())
@@ -383,10 +395,12 @@ def AQUASTATData():
     data = data.drop(['Variable Id', 'Area Id', 'Symbol', 'Md'], axis=1)
 
     data = data.dropna(how='any')
-    p = pd.pivot_table(data, index=['Country Name in IFs', 'Year'], columns=['Series name in IFs'], values=['Value'],
+    data = pd.pivot_table(data, index=['Country Name in IFs', 'Year'], columns=['Series name in IFs'], values=['Value'],
                        aggfunc=[np.sum])
-
-    return (p)
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'Value',", "").replace(")", "").replace("'", "") \
+                    for hdr in data.columns]
+    return (data)
 
 
 def AQUASTATDataFile():
@@ -402,8 +416,8 @@ def IMFGFSRevenueData():
     import numpy as np
     import pandas as pd
     import dask.dataframe as dd
-    concord_table = pd.read_excel('C:\\Users\Public\Pythonfiles\CountryConcordanceIMF.xlsx')
-    data = pd.read_csv('C:\\Users\Public\Pythonfiles\GFSRevenue.csv',chunksize=100000)
+    concord_table = pd.read_excel('input\CountryConcordanceIMF.xlsx')
+    data = pd.read_csv('input\GFSRevenue.csv',chunksize=100000)
     chunk_list=[]
     for chunk in data:
         chunk['FuncSector'] = chunk[str('Sector Name')] + chunk[str('Classification Name')]
@@ -411,7 +425,7 @@ def IMFGFSRevenueData():
         chunk=chunk.rename(columns={"Time Period":"Year"})
         chunk = chunk.loc[chunk['Unit Name'] == 'Percent of GDP']
         chunk.dropna(how='any')
-        print(chunk.head())
+
         chunk_list.append(chunk)
     data=pd.concat(chunk_list)
 
@@ -423,10 +437,12 @@ def IMFGFSRevenueData():
 
     #data = data.reset_index()
 
-    p = pd.pivot_table(data, index=["Country name in IFs", "Unit Name", 'Year'], values=['Value'],
+    data = pd.pivot_table(data, index=["Country name in IFs", "Unit Name", 'Year'], values=['Value'],
                        columns=['FuncSector'], aggfunc=[np.sum])
-
-    return (p)
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'Value',", "").replace(")", "").replace("'", "") \
+                    for hdr in data.columns]
+    return (data)
 
 
 def IMFGFSRevenueDataFile():
@@ -461,21 +477,23 @@ def IMFGFSExpenditureData():
     import xlsxwriter
     import statsmodels.api as sm
 
-    data = pd.read_csv('C:\\Users\Public\Pythonfiles\GFSCOFOG_02-23-2018 23-30-06-28.csv')
+    data = pd.read_csv('input\GFSCOFOG_02-23-2018 23-30-06-28.csv')
 
     data['FuncSector'] = data[str('COFOG Function Name')] + data[str('Sector Name')]
-    print(data.head())
 
-    concord_table = pd.read_excel('C:\\Users\Public\Pythonfiles\CountryConcordanceIMF.xlsx')
-    print(concord_table.head())
+
+    concord_table = pd.read_excel('input\CountryConcordanceIMF.xlsx')
+
 
     data = data.merge(concord_table, on="Country Name", how='left')
-    print(data.head())
 
-    p = pd.pivot_table(data, index=["Country name in IFs", "Unit Name", 'Time Period'], values=['Value'],
+
+    data = pd.pivot_table(data, index=["Country name in IFs", "Unit Name", 'Time Period'], values=['Value'],
                        columns=['FuncSector'], aggfunc=[np.sum])
-
-    return (p)
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'Value',", "").replace(")", "").replace("'", "") \
+                    for hdr in data.columns]
+    return (data)
 
 
 def IMFGFSExpenditureDataFile():
@@ -515,26 +533,28 @@ def WDIData():
     import dask.dataframe as dd
 
     # Step 1: First read in the excel/csv with the relevant WDI codes and print out the WDI codes to be sure we have the right number of values
-    datadict = pd.read_excel('C:\\Users\Public\Pythonfiles\WDICodes.xlsx', sheet_name='Sheet1',
+    datadict = pd.read_excel('input\WDICodes.xlsx', sheet_name='Sheet1',
                              dtype='str')  # Replace the location of code files for your desktop
     print('Hey User!, you are importing ' + (str(len(datadict[
                                                          'WDI Code'])) + ' series. To import additional series please update the file WDICodes.xlsx located in the Pythonfiles folder'))
 
     # print(datadict['WDI Code'])
-
+    countryconcord = pd.read_csv('input\CountryConcordanceWDI.csv', encoding="ISO-8859-1")
+    seriesconcord = pd.read_csv('input\CodeConcordanceWDI.csv', encoding="ISO-8859-1")
     print('Step 1 Complete: All files read in')
 
     # Step 2: Prepare columns for series name,country,year,value
     Seriesname = []
-    Countryname = []
-    SeriesCode = []
-    Year = []
-    Value = []
 
+    df=[]
     print(
         'We will be starting the loop for all codes now.')
     # Step 3: Create a loop for reading in all the codes into the url and parsing the xml file for relevant values.This helps in pulling in 500 series at a time
     for row in datadict['WDI Code']:
+        Countryname = []
+        SeriesCode = []
+        Year = []
+        Value = []
         wiki = 'http://api.worldbank.org/v2/countries/all/indicators/' + str(row) + '/?format=xml&per_page=20000'
         r = requests.get(wiki, stream=True)
         root = et.fromstring(r.content)
@@ -547,40 +567,31 @@ def WDIData():
             Year.append(child.text)
         for child in root.iter("{http://www.worldbank.org}value"):
             Value.append((child.text))
-    print('Loop is complete.Hard part is over now!')
+        test_df = pd.DataFrame.from_dict({'SeriesName': SeriesCode,
+                                          'Country': Countryname,
+                                          'Year': Year,
+                                          'Value': Value}, orient='index')
+        test_df = test_df.transpose()
+        test_df = pd.merge(test_df, countryconcord, how='left', left_on='Country', right_on='Country')
+        test_df = pd.merge(test_df, seriesconcord, how='left', left_on='SeriesName', right_on='CodeinIfs')
+        test_df = test_df[pd.notnull(test_df['Value'])]
+        test_df = test_df[pd.notnull(test_df['Country name in IFs'])]
+        test_df = test_df[pd.notnull(test_df['Series'])]
 
-    # Step 4: Write all the parsed values to the dataframe in Step 2
-    test_df = pd.DataFrame.from_dict({'SeriesName': SeriesCode,
-                                      'Country': Countryname,
-                                      'Year': Year,
-                                      'Value': Value}, orient='index')
-    print('Step 4 complete! You have a data frame now!')
-    # Step 5: Read in concordance tables for Countries and series
-    countryconcord = pd.read_csv('C:\\Users\Public\Pythonfiles\CountryConcordanceWDI.csv', encoding="ISO-8859-1")
-    seriesconcord = pd.read_csv('C:\\Users\Public\Pythonfiles\CodeConcordanceWDI.csv', encoding="ISO-8859-1")
+        df.append(test_df)
 
-    print('Step 5 complete! We have read in the concordance tables')
-    # Step 6: Create a transponsed file using the dataframe
-    df = test_df.transpose()
 
-    print('Step 6 complete! We have a transposed dataset!')
 
-    # Step 7:Concord country and series names
-    df = pd.merge(df, countryconcord, how='left', left_on='Country', right_on='Country')
-    df = pd.merge(df, seriesconcord, how='left', left_on='SeriesName', right_on='CodeinIfs')
+    df=pd.concat(df)
 
-    print('Step 7 complete! We have a merged dataset now!')
 
-    # Step 8: Drop null values to keep data managable
-    df = df[pd.notnull(df['Value'])]
-    df = df[pd.notnull(df['Country name in IFs'])]
-    df = df[pd.notnull(df['Series name in IFs'])]
-
-    print('We have dropped all null values!')
-    # Step 9: Write to a pivot table
-    data = pd.pivot_table(df, index=['Country name in IFs', 'CodeinIfs'], columns=['Year'], values=['Value'],
+     # Step 9: Write to a pivot table
+    data = pd.pivot_table(df, index=['Country name in IFs', 'Year'], columns=['Series'], values=['Value'],
                           aggfunc=[np.sum])
-
+    data = pd.DataFrame(data.to_records())
+    data.columns = [hdr.replace("('sum', 'Value',", "").replace(")", "").replace("'", "") \
+                   for hdr in data.columns]
+    #data= df
     return (data)
 
 
